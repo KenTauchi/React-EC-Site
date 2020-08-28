c97f4383b43d4300635a12afa63f9271801c8783
Ken Tauchi <kentauchica@gmail.com>
2020-08-28 15:33:58 -0700

    collection page style and display its components

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shop";
 import { Switch, Route } from "react-router-dom";
 
-const HatsPage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  );
-};
+// const HatsPage = (props) => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <h1>HATS PAGE</h1>
+//     </div>
+//   );
+// };
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
