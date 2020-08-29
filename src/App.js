8a9dc610f60e3e590ec4bc260584db316af3b5e5
Ken Tauchi <kentauchica@gmail.com>
2020-08-28 21:05:07 -0700

    style button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop";
 import Header from "./components/header/header";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 import { Switch, Route } from "react-router-dom";
 
 // const HatsPage = (props) => {
@@ -22,6 +23,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
